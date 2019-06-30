d6a30278a4250d5e1f09f62e82d69a3c8732c94b
Salvador Murillo <chavitation@gmail.com>
2019-06-29 21:32:05 -0700

    created buttons that generate gifs when clicked

---
+++ javascript/javascript.js
@@ -0,0 +1,41 @@
+var queryURL;
+var buttons = [];
+
+function pushValueToArray() {
+
+    var value = $("#input").val();
+    buttons.push(value);
+    console.log(buttons);
+    $("#buttonsView").empty();
+    for (var i = 0; i < buttons.length; i++) {
+        var newButton = $('<input/>').attr({ type: 'button', value: buttons[i], class: "btn007" });
+        newButton.attr("data-name",buttons[i]);
+        newButton.appendTo("#buttonsView");
+    }
+
+    $(".btn007").on("click", function () {
+        var keyword = $(this).attr("data-name")
+        console.log(keyword);
+    
+        queryURL = "https://api.giphy.com/v1/gifs/search?q=" + keyword + "&api_key=xwqYvqzFELtq3Tld178aq1bwGuPS8BC8&limit=10";
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var data = response.data;
+            for (var i = 0; i < data.length; i++) {
+                var url = data[i].images.fixed_height.url;
+                var img = $("<img>");
+                img.attr("src", url);
+                $("#gifsHere").append(img);
+            }
+    
+        })
+    })
+
+}
+
+
+
+
